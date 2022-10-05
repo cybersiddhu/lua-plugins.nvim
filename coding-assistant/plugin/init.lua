fce053577973f89971f5c505572e6b460a125d4c
Siddhartha Basu <biosidd@gmail.com>
2022-10-05 07:51:02 -0500

    feat: add a setup function to setup the plugin

--- coding-assistant/plugin/init.lua
+++ coding-assistant/plugin/init.lua
@@ -1,2 +1,7 @@
-local stackoverflow = require("coding-assistant.stackoverflow")
-vim.keymap.set("n", "<Leader>so", function() stackoverflow.so_search() end, { noremap = true })
+local M = {}
+function M.setup()
+  local stackoverflow = require("coding-assistant.stackoverflow")
+  vim.keymap.set("n", "<Leader>so", function() stackoverflow.so_search() end, { noremap = true })
+end
+
+return M
